9a32056768fabaf2784b818b104aa0b9c4caec89
1abcdesign <one.abcdesign@gmail.com>
2024-11-13 15:44:02 +0200

    Updates

---
+++ static/js/156.fda0abbe.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_web_dev_basics=self.webpackChunkreact_web_dev_basics||[]).push([[156],{156:function(s,e,c){c.r(e),c.d(e,{default:function(){return i}});c(791);var t=c(184),i=function(){return(0,t.jsxs)("div",{className:"section success block",id:"sign-up-page",children:[(0,t.jsx)("p",{className:"text-title",children:"User successfully registered"}),(0,t.jsx)("div",{className:"success-picture"})]})}}}]);
+//# sourceMappingURL=156.fda0abbe.chunk.js.map
